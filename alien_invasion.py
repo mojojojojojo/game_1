2b550b5798f67b9efdb5a986af1715469a744b74
Moritz Boell <momo.boell@yahoo.de>
2016-11-26 13:25:28 +0100

    shoot_bullets and moving accomplished

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
-
+from pygame.sprite import Group
 
 def run_game():
     pygame.init()
@@ -12,10 +12,13 @@ def run_game():
 
     pygame.display.set_caption("Alien invasion")
     ship = Ship(ai_settings, screen)
+    bullets = Group()
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
