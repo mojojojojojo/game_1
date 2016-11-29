465460968e56c13bd2825ded72cfadd94ee99655
Moritz Boell <momo.boell@yahoo.de>
2016-11-29 23:19:44 +0100

    shoots and hits

--- alien.py
+++ alien.py
@@ -12,7 +12,7 @@ class Alien(Sprite):
         self.ai_settings = ai_settings
 
         # load the image and set its rect attribute.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
         # new alien at top left of the screen.
@@ -25,3 +25,14 @@ class Alien(Sprite):
     def blitme(self):
         """Draw the alien at teh current location"""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        self.x += (self.ai_settings.alien_speed_factor * self.ai_settings.fleet_direction)
+        self.rect.x = self.x
+
+    def check_edges(self):
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from alien import Alien
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
@@ -14,12 +15,15 @@ def run_game():
     pygame.display.set_caption("Alien invasion")
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
-        ship.update()
 
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        ship.update()
+        gf.update_bullets(aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
